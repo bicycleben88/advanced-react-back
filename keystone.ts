9b86ceae9e7a1966609c238cf76361a254f1bffe
Ben Higginbotham <bicycleben88@gmail.com>
2021-02-05 09:57:06 -0500

    keystone & mongo config

---
+++ keystone.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/sicks-fitz";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long they stayed longed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL,
+    //todo add seeding data here
+  },
+  lists: createSchema({
+    //schema items go in here
+  }),
+  ui: {
+    // change this for roles
+    isAccessAllowed: () => true,
+  },
+  // todo add session values here
+});
